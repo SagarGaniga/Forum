388bdc444f09e638e41805a44a3651eafe89fe33
shravanjain <shravanjain1996@gmail.com>
2017-04-22 00:14:36 +0530

    Add files via upload

--- categories.php
+++ categories.php
@@ -3,14 +3,22 @@
         <h4 style="color:grey">Categories</h4><hr>
         <?php
             require("dbCon.php");
-            $sql = "select cat_name from categories where 1";
+            $sql = "select cat_name,cat_id from categories where 1";
             $result = $conn->query($sql);
             if ($result->num_rows > 0) 
             {
                 while($row = $result->fetch_assoc()) 
                 {
                     echo "<a href=\"#\">".$row["cat_name"]."</a>";
-                    echo "<br><br>";   
+                    $id=$row["cat_id"];
+                    $sql = "select topic_name from topic where topic_cat='$id'";
+                    $res = $conn->query($sql);
+                    while($raw=$res->fetch_assoc())
+                    {
+                        echo "<br>";
+                        echo "<a href=\"#\">".$raw["topic_name"]."</a>";
+                    }
+                    echo "<br>";   
                 }
             }
         ?>
